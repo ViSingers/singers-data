1fd34256fa3143ac20a1240f29e1b77add3d5bce
Megageorgio <korogodoff@yandex.ru>
2026-04-12 23:29:52 +0400

    Removed unnecessary properties

--- parser.js
+++ parser.js
@@ -490,10 +490,8 @@ async function main() {
 
             for (const group of finalGroups) {
                 const groupRepoUrl = `${group.repositoryPath.toLowerCase()}`;
-                console.log(groupRepoUrl, singerRepoUrl);
                 const singerMentionsGroup = declaredUrls.includes(groupRepoUrl);
                 const groupMentionsSinger = group.memberUrls?.includes(singerRepoUrl);
-                console.log(singerMentionsGroup, groupMentionsSinger);
                 if (singerMentionsGroup && groupMentionsSinger) {
                     group.participants.push({
                         id: singer.id,
@@ -507,6 +505,9 @@ async function main() {
                         repositoryName: group.repositoryName
                     });
                 }
+
+                delete group.memberUrls;
+                delete singer.declaredGroupUrls;
             }
         }
 
